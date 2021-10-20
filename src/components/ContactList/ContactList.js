1496a9c2b751b6dab00589480a73ff5600c561d2
Anton Kovalchuck <antonkovalchuck@gmail.com>
2021-10-20 19:33:06 +0300

    hooks upd

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,29 @@
+import { PropTypes } from "prop-types";
+import styles from "./ContactList.module.css";
+import { v4 as uuidv4 } from "uuid";
+
+export function ContactList({ filteredContacts, onListChange }) {
+  return (
+    <ul className={styles.contact_list}>
+      {filteredContacts.map(({ name, number, id }) => (
+        <li className={styles.contact_list_item} key={uuidv4()}>
+          <p>
+            {name}: {number}
+          </p>
+          <button
+            className={styles.contact_list_button}
+            type="button"
+            onClick={() => onListChange(id)}
+          >
+            Delete number
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  state: PropTypes.arrayOf(PropTypes.any).isRequired,
+  onListChange: PropTypes.func.isRequired,
+};
